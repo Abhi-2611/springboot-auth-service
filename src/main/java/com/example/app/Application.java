e87fc7cfc64d64fcca12f38290885328176d7b63
Abhi <abhimusale2631@gmail.com>
2026-01-22 18:15:48 +0530

    refactor(structure): rename application entry point

---
+++ src/main/java/com/example/app/Application.java
@@ -0,0 +1,19 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+
+		System.out.println("============================================================");
+        System.out.println(" School-Management-System is running successfully");
+        System.out.println("============================================================");
+	}
+
+}
